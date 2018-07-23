b849b2c0ae1ca61724420c21106dac5c3539e983
coderyangjie <327409251@qq.com>
2018-07-23 09:54:24 +0800

    引入测试js

---
+++ examples/js/dateUtils.js
@@ -0,0 +1,56 @@
+/**
+ * Created by yangjie on 2018/7/20.
+ * 日期处理工具类（使用闭包方式开发）
+ */
+
+;//JavaScript 弱语法的特点,如果前面刚好有个函数没有以";"结尾,那么可能会有语法错误，因此在这里加上";"号。
+//使用自调用匿名函数封装(防止插件用户定义的函数名与插件名冲突)
+(function(window,document){
+    //定义一些默认参数
+    var _options={
+        default_word:"default hello"
+
+    };
+
+    //定义一些API方法
+    var _DateUtils = {
+        dateFormat:_dateFormat
+    };
+
+
+    /**
+     * 日期格式化，第一个参数为日期，第二个参数为日期格式化的格式，返回一个格式化后的字符串
+     */
+    function _dateFormat(date, format) {
+        var o = {
+            "M+": date.getMonth() + 1, //month
+            "d+": date.getDate(), //day
+            "h+": date.getHours(), //hour
+            "m+": date.getMinutes(), //minute
+            "s+": date.getSeconds(), //second
+            "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+            "S": date.getMilliseconds() //millisecond
+        };
+        if (/(y+)/.test(format)) {
+            format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+            }
+        }
+        return format;
+    }
+
+
+
+
+
+
+
+    //这里确定了插件的名称
+    //this.dateUtils = _DateUtils;
+    window.dateUtils = _DateUtils;
+
+}(window,document));
