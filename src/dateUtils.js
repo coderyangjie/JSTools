87e96e7df7dc6e7ca11a13893da3365e3640e6b7
coderyangjie <327409251@qq.com>
2018-07-23 09:55:37 +0800

    日期处理工具类

--- src/dateUtils.js
+++ src/dateUtils.js
@@ -8,14 +8,46 @@
 (function(window,document){
     //定义一些默认参数
     var _options={
-        default_word:"default hello"
+        parameterName:"parameterValue"
 
     };
 
     //定义一些API方法
     var _DateUtils = {
-
+        dateFormat:_dateFormat
     };
+
+
+    /**
+     * 日期格式化，第一个参数为日期，第二个参数为日期格式化的格式，返回一个格式化后的字符串
+     */
+    function _dateFormat(date, format) {
+        var o = {
+            "M+": date.getMonth() + 1, //month
+            "d+": date.getDate(), //day
+            "h+": date.getHours(), //hour
+            "m+": date.getMinutes(), //minute
+            "s+": date.getSeconds(), //second
+            "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+            "S": date.getMilliseconds() //millisecond
+        };
+        if (/(y+)/.test(format)) {
+            format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+            }
+        }
+        return format;
+    }
+
+
+
+
+
+
 
     //这里确定了插件的名称
     //this.dateUtils = _DateUtils;
