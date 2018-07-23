f9e07eb6b1bf4c554135fc0910bf82cc5821e03e
coderyangjie <327409251@qq.com>
2018-07-23 14:18:12 +0800

    添加新API

--- examples/js/dateUtils.js
+++ examples/js/dateUtils.js
@@ -23,7 +23,8 @@
         getFirstDayOfYear:_getFirstDayOfYear,
         getLastDayOfYear:_getLastDayOfYear,
         computeTotalMonths:_computeTotalMonths,
-        dateStringToChinese:_dateStringToChinese
+        dateStringToChinese:_dateStringToChinese,
+        dateDiff:_dateDiff
 
     };
 
@@ -251,6 +252,24 @@
             dayStr = dict[dd];
 
         return yearStr+monthStr+dayStr;
+    }
+
+
+    /**
+     * 计算两个日期相差的天数
+     * @param sDate1
+     * @param sDate2
+     * @returns {Number|*}
+     * @private
+     */
+    function  _dateDiff(sDate1,  sDate2){    //sDate1和sDate2是2002-12-18格式
+        var  aDate,  oDate1,  oDate2,  iDays;
+        aDate  =  sDate1.split("-");
+        oDate1  =  new  Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);    //转换为12-18-2002格式
+        aDate  =  sDate2.split("-");
+        oDate2  =  new  Date(aDate[1] +  '-'  + aDate[2] +  '-' + aDate[0]);
+        iDays  =  parseInt(Math.abs(oDate1 - oDate2)/1000/60/60/24);    //把相差的毫秒数转换为天数
+        return  iDays;
     }
 
 
